d569951d9e855104c879cec67628f28ae053bc7a
akasomya <somyad841@gmail.com>
2024-06-26 22:09:55 -0400

    chore: Add input validation for IPv4 address parts

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String ip_address;
+        Scanner k = new Scanner(System.in);
+    
+     
+        
+        System.out.println("Enter IPv4 Address: ");
+        ip_address = k.next();
+
+        
+
+        for (String parts: ip_address.split("\\.")) {
+            
+            if (Integer.parseInt(parts) > 255){
+                System.out.println("The value [" +  parts + "] at the index " + "");
+            }
+                       
+        }
+
+        k.close();
     }
 }
