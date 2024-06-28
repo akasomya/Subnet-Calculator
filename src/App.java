c59952bc69ca04486627a7f0f5d134715bfafa61
akasomya <somyad841@gmail.com>
2024-06-27 22:27:42 -0400

    Refactor input validation for IPv4 address parts

--- src/App.java
+++ src/App.java
@@ -4,20 +4,18 @@ public class App {
     public static void main(String[] args) {
         String ip_address;
         Scanner k = new Scanner(System.in);
-    
+        int i  = 0;
      
-        
-        System.out.println("Enter IPv4 Address: ");
-        ip_address = k.next();
-
-        
+        System.out.print("Enter IPv4 Address: ");
+        ip_address = k.next();        
 
         for (String parts: ip_address.split("\\.")) {
-            
-            if (Integer.parseInt(parts) > 255){
-                System.out.println("The value [" +  parts + "] at the index " + "");
-            }
-                       
+            ++i;
+
+            if(i <= 4){
+                if(Integer.parseInt(parts) > 255){System.out.println("The value [" +  parts + "] at the octal " + i + " is out of range");} 
+            }         
+                
         }
 
         k.close();
