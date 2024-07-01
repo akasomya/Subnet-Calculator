e3abe01f448d44b840d56b5c9cac70c59f4fe6f5
akasomya <somyad841@gmail.com>
2024-07-01 13:38:12 -0400

    Refactor input validation for IPv4 address parts

--- src/App.java
+++ src/App.java
@@ -1,26 +1,25 @@
 import java.util.Scanner;
 public class App{
 
-    
-
-    String Four_Octet_Check(String ip_address_input){
-
-        return ip_address_input;
-    
-        
-    
-    }
-
 public static void main(String[] args) {
     String ip_address_input;
 
-    App obj = new App();
+    Input_Validation obj = new Input_Validation();
     Scanner k = new Scanner(System.in);
 
+    System.out.print("Enter IPv4 Address: ");
     ip_address_input = k.next();
 
-    String x = obj.Four_Octet_Check(ip_address_input);
-    System.out.println(k);
+    if (obj.get_Four_Octet_Check_Validation(ip_address_input) == true){
+        System.out.println("IP Valid");
+
+    }
+    else{System.out.println("Ip not Valid");}
+
+
+// String x = "The value [" +  ip_address_parts_for_check + "] at the octal " + i + " is out of range";
+                    
+
     k.close();
 
 
--- src/Input_Validation.java
+++ src/Input_Validation.java
@@ -1,7 +1,27 @@
 class Input_Validation{
+    int i = 0;
+    boolean x;
 
-public String ip_address_input;
+    public Boolean get_Four_Octet_Check_Validation(String ip_address_input){        
+    
+        for (String ip_address_parts: ip_address_input.split("\\.")) {
+            ++i;
+            if (i == 4){
 
+                for (String ip_address_parts_for_check: ip_address_input.split("\\.")){
+                    int octet = Integer.parseInt(ip_address_parts_for_check);
+                    if(octet < 0 || octet > 255){
+                    
+                    x = false;
+                } else{x = true;}
 
+                }   
+        
+        }else{x = false;}  
+        
+    }
+    return x;       
+    
+}
 
 }
